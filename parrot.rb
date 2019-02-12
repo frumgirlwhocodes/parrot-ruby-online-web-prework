a2d78ac0f2b35d29ab2f94460dfb8bb5694156b5
lieby weintraub <liebahinda@gmail.com>
2019-02-12 23:07:33 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot=("squawk")
-  puts "squawk!"
+def parrot(head="Squawk!")
+  puts "Squawk!"
+  parrot
 end 
 
